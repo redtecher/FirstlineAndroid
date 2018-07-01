997336a0430243d62bf693bbaf91a1122e704901
Hong Chuan <hongchuan95@live.com>
2018-07-01 09:05:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/thinkpad/firstlineandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.thinkpad.firstlineandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"Oncreate the activity",Toast.LENGTH_SHORT).show();
+
+    }
+}
